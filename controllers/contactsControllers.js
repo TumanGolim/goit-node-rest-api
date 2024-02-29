1e3412bf4e799a446f7bd48d7e7a3c526425c5e2
TumanGolim <130469755+TumanGolim@users.noreply.github.com>
2024-02-29 19:01:38 +0200

    fix

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,54 +1,22 @@
-import * as contactsService from "../services/contactsServices.js";
+import express from "express";
+import { updateStatusContact } from "../services/contactsServices.js";
 
-export const getAllContacts = async (req, res) => {
+const router = express.Router();
+
+router.patch("/:contactId/favorite", async (req, res) => {
+  const { contactId } = req.params;
+  const { favorite } = req.body;
+
   try {
-    const contacts = await contactsService.listContacts();
-    res.status(200).json(contacts);
+    const updatedContact = await updateStatusContact(contactId, favorite);
+    if (updatedContact) {
+      res.status(200).json(updatedContact);
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-};
+});
 
-export const getOneContact = async (req, res) => {
-  const { id } = req.params;
-  try {
-    const contact = await contactsService.getContactById(id);
-    res.status(200).json(contact);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-export const deleteContact = async (req, res) => {
-  const { id } = req.params;
-  try {
-    await contactsService.removeContact(id);
-    res.status(204).end();
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-export const createContact = async (req, res) => {
-  const { name, email, phone } = req.body;
-  try {
-    const newContact = await contactsService.addContact({ name, email, phone });
-    res.status(201).json(newContact);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
-
-export const updateContact = async (req, res) => {
-  const { id } = req.params;
-  const updatedFields = req.body;
-  try {
-    const updatedContact = await contactsService.updateContact(
-      id,
-      updatedFields
-    );
-    res.status(200).json(updatedContact);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-};
+export default router;
